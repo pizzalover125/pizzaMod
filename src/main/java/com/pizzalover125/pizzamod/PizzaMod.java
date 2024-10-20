b2ad8efd0109e0f670d171d8a8e61ff1a942be47
pizzalover125 <119770527+pizzalover125@users.noreply.github.com>
2024-10-20 16:19:16 -0700

    Added items

--- src/main/java/com/pizzalover125/pizzamod/PizzaMod.java
+++ src/main/java/com/pizzalover125/pizzamod/PizzaMod.java
@@ -1,9 +1,16 @@
 package com.pizzalover125.pizzamod;
 
+import com.pizzalover125.pizzamod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class PizzaMod implements ModInitializer {
 	public static final String MOD_ID = "pizzamod";
@@ -11,6 +18,6 @@ public class PizzaMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/pizzalover125/pizzamod/item/ModItems.java
@@ -0,0 +1,27 @@
+package com.pizzalover125.pizzamod.item;
+
+import com.pizzalover125.pizzamod.PizzaMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PIZZA = registerItem("pizza", new Item(new Item.Settings()));
+    public static final Item PIZZA_SLICE = registerItem("pizza_slice", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(PizzaMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        PizzaMod.LOGGER.info("Registering Mod Items for " + PizzaMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PIZZA);
+            entries.add(PIZZA_SLICE);
+        });
+    }
+}

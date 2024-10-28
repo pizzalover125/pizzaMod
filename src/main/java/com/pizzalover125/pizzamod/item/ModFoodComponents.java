c56be41184c85602ca1b0c47edbd5584c37c15f3
pizzalover125 <119770527+pizzalover125@users.noreply.github.com>
2024-10-28 06:21:42 -0700

    Minor crafting+food bug fixes

--- src/main/java/com/pizzalover125/pizzamod/item/ModFoodComponents.java
+++ src/main/java/com/pizzalover125/pizzamod/item/ModFoodComponents.java
@@ -4,19 +4,17 @@ import net.minecraft.component.type.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent PIZZA = new FoodComponent.Builder()
-            .nutrition(6)
+            .nutrition(12)
             .saturationModifier(1.2f)
             .build();
 
     public static final FoodComponent PIZZA_SLICE = new FoodComponent.Builder()
-            .nutrition(2)
-            .saturationModifier(0.4f)
+            .nutrition(6)
+            .saturationModifier(0.6f)
             .build();
 
     public static final FoodComponent CHEESE_SLICE = new FoodComponent.Builder()
-            .nutrition(2)
+            .nutrition(4)
             .saturationModifier(0.4f)
             .build();
-
-
 }
